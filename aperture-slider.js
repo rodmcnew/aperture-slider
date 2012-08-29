4c3f208b05af0a32082a0cd7df90ee84042e99fe
Rod Mcnew <rmcnew@relivinc.com>
2012-08-29 10:19:52 -0500

    cleanup code

--- aperture-slider.js
+++ aperture-slider.js
@@ -9,45 +9,47 @@
  */
 var ApertureSlider = function (apertureDiv, width, frameCount) {
 
-    var filmDiv = apertureDiv.children('div');
+    //Config options
+    var animationDelay = 400;
+    var frameSeparation = 100;//how far apart the frame are in pixels
 
+    //Get all the divs we need to work with
+    var filmDiv = apertureDiv.children('div');
     var frameDivs = filmDiv.children('div');
 
-    var frameSeparation = 100;
-
+    //Init var
     var currentFrame = 0;
 
+    //Add css
     frameDivs.css('float', 'left');
     frameDivs.css('width', width + 'px');
     frameDivs.css('margin-right', frameSeparation + 'px');
-
     filmDiv.css('width', +(frameCount * (width + frameSeparation)) + 'px');
     filmDiv.css('margin-left: 0');
-
     apertureDiv.css('width', width + 'px');
     apertureDiv.css('overflow', 'hidden');
     apertureDiv.show();
 
-    this.setCurrentFrame = function (frameIndex) {
+    this.setCurrentFrame = function (frameIndex, callBack) {
         currentFrame = frameIndex;
-        this.render();
+        this.render(callBack);
     }
 
     this.getCurrentFrame = function () {
         return currentFrame;
     }
 
-    this.goForward = function () {
+    this.goForward = function (callBack) {
         if (currentFrame < frameCount - 1) {
             currentFrame += 1
-            this.render();
+            this.render(callBack);
         }
     }
 
-    this.goBack = function () {
+    this.goBack = function (callBack) {
         if (currentFrame != 0) {
             currentFrame -= 1;
-            this.render();
+            this.render(callBack);
         }
     }
 
@@ -56,7 +58,7 @@ var ApertureSlider = function (apertureDiv, width, frameCount) {
             {
                 'margin-left':-currentFrame * (width + frameSeparation)
             },
-            400,
+            animationDelay,
             callBack
         )
     }
