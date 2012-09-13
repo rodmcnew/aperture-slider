613fa7fc890be5cc9a173787ec70df954bf9b6ea
Rod Mcnew <rmcnew@relivinc.com>
2012-09-13 11:30:03 -0500

    added support for browser's back, forward, and refresh buttons

--- aperture-slider.js
+++ aperture-slider.js
@@ -7,11 +7,28 @@
  * @author    Rod McNew <rodmcnew@gmail.com>
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-var ApertureSlider = function (apertureDiv, width, frameCount) {
 
-    //Config options
+/**
+ * Aperture Slider Constructor
+ *
+ * @param {jQuery Object} apertureDiv
+ * @param {Integer} frameCount
+ * @param {Integer} width
+ * @param {Integer} minHeight [optional]
+ * @param {jQuery Object} autoHidingBackButton [optional]
+ * @param {Integer} animationDelay [optional]
+ * @param {Integer} frameSeparation [optional]
+ * @constructor
+ */
+var ApertureSlider = function (apertureDiv, frameCount, width, minHeight) {
+    //Default Values
+    if (typeof(minHeight) == 'undefined') {
+        minHeight = 300;
+    }
+
     var animationDelay = 400;
-    var frameSeparation = 100;//how far apart the frame are in pixels
+    var frameSeparation = 100;
+
 
     //Get all the divs we need to work with
     var filmDiv = apertureDiv.children('div');
@@ -19,47 +36,226 @@ var ApertureSlider = function (apertureDiv, width, frameCount) {
 
     //Init var
     var currentFrame = 0;
+    var frameChangedCallBack;
+    var browserButtonSupportEnabled=false;
+    var browserButtonUrlName;
 
     //Add css
     frameDivs.css('float', 'left');
     frameDivs.css('width', width + 'px');
+    frameDivs.css('min-height', minHeight + 'px');
     frameDivs.css('margin-right', frameSeparation + 'px');
     filmDiv.css('width', +(frameCount * (width + frameSeparation)) + 'px');
     filmDiv.css('margin-left: 0');
     apertureDiv.css('width', width + 'px');
     apertureDiv.css('overflow', 'hidden');
-    apertureDiv.show();
 
-    this.setCurrentFrame = function (frameIndex, callBack) {
-        currentFrame = frameIndex;
-        this.render(callBack);
+    //Hide off-screen frame contents
+    frameDivs.children().hide();
+    $(frameDivs.get(0)).children().show();
+
+    /**
+     * Always refers to me object unlike the 'me' JS variable;
+     *
+     * @type {ApertureSlider}
+     */
+    var me = this;
+
+
+    /**
+     * Sets the current frame. This is the meat of this class.
+     *
+     * @param {Integer} frameIndex the frame that we want to switch to
+     * @param {Function} callBack [optional] is called when sliding is complete
+     * @param {Boolean} skipPushState [optional] used internally only
+     */
+    me.setCurrentFrame = function (frameIndex, callBack, skipPushState) {
+
+        if (currentFrame == -1) {
+            //don't allow more sliding if we are already in the middle of a slide
+            return false;
+        } else if(currentFrame==frameIndex) {
+            //If we are already here, there is no reason to move
+            if(typeof(callBack)=='function'){
+                callBack(currentFrame);
+            }
+        } else {
+            //Save the last frame that we were on so we can hide it after the
+            //transition
+            var lastFrame = currentFrame;
+
+            //mark that we are currently sliding
+            currentFrame = -1;
+
+
+            //Show the next frame's contents
+            $(frameDivs.get(frameIndex)).children().show();
+
+            //Mess with the url if browser button support is on
+            if(browserButtonSupportEnabled){
+                if(!skipPushState){
+                    me.pushStateToHistory(frameIndex);
+                }
+            }
+
+            filmDiv.animate(
+                {
+                    'margin-left':-frameIndex * (width + frameSeparation)
+                },
+                animationDelay,
+                function () {
+                    //hide the previous frame's contents
+                    $(frameDivs.get(lastFrame)).children().hide();
+
+                    //mark that we are done sliding
+                    currentFrame = frameIndex;
+
+                    //call the passed-in callback if it is set
+                    if (typeof(callBack) == 'function') {
+                        callBack(currentFrame);
+                    }
+
+                    //call universal frame-changed callBack if it is set
+                    if (typeof(frameChangedCallBack) != 'undefined') {
+                        frameChangedCallBack(currentFrame);
+                    }
+
+
+
+                }
+            )
+        }
+
     }
 
-    this.getCurrentFrame = function () {
+    /**
+     * Sets a callback function that will be call after each frame change
+     * completes.
+     *
+     * @param {Function} callBack [optional] is called when sliding is complete
+     */
+    me.setFrameChangedCallBack = function (callBack) {
+        frameChangedCallBack = callBack;
+    }
+
+    /**
+     * Returns which frame we are currently on
+     *
+     * @return {Number}
+     */
+    me.getCurrentFrame = function () {
         return currentFrame;
     }
 
-    this.goForward = function (callBack) {
+    /**
+     * slide to next frame
+     *
+     * @param {Function} callBack [optional] is called when sliding is complete
+     */
+    me.goForward = function (callBack) {
         if (currentFrame < frameCount - 1) {
-            currentFrame += 1
-            this.render(callBack);
+            me.setCurrentFrame(currentFrame + 1, callBack);
         }
     }
 
-    this.goBack = function (callBack) {
+    /**
+     * Slide to last frame
+     *
+     * @param {Function} callBack [optional] is called when sliding is complete
+     */
+    me.goBack = function (callBack) {
         if (currentFrame != 0) {
-            currentFrame -= 1;
-            this.render(callBack);
+            me.setCurrentFrame(currentFrame - 1, callBack);
         }
     }
 
-    this.render = function (callBack) {
-        filmDiv.animate(
-            {
-                'margin-left':-currentFrame * (width + frameSeparation)
-            },
-            animationDelay,
-            callBack
-        )
+    /**
+     * Gets the number of frames
+     *
+     * @return {Number}
+     */
+    me.getFrameCount = function () {
+        return frameCount;
+    }
+
+    /**
+     * Enables support for the browser's back and refresh buttons
+     *
+     * @param {String} urlName the url parameter name to use to store the
+     * current frame. Example: "step"
+     */
+    me.enableBrowserButtonSupport = function(urlName){
+
+        if (typeof(urlName) == 'undefined') {
+            urlName = 'frame';
+        }
+
+        browserButtonUrlName = urlName;
+        browserButtonSupportEnabled = true;
+
+        $(window).bind('popstate', function(event) {
+                var urlParams=me.getUrlParams();
+                var frame = urlParams[browserButtonUrlName];
+                if(!me.isNumeric(frame)){
+                    frame=0;
+                }
+                me.setCurrentFrame(
+                    parseFloat(frame),null,true
+                );
+        })
+    }
+
+    /**
+     * Pushes the current state (which frame we are on) to the html5 history
+     * object. This is used for browser button support.
+     *
+     * @param frame
+     */
+    me.pushStateToHistory = function(frame){
+        history.pushState(
+            {apertureSlider:browserButtonUrlName},
+            null,
+            me.getUrlWithoutParams() + '?'+browserButtonUrlName+'=' + frame
+        );
+    }
+
+    /**
+     * Checks if a value is numeric
+     *
+     * @param value
+     * @return {Boolean}
+     */
+    me.isNumeric = function(value){
+        return !isNaN(value)&&isFinite(value);
+    }
+
+    /**
+     * Gets the url without the '?' query string
+     *
+     * @return {String}
+     */
+    me.getUrlWithoutParams = function(){
+        return window.location.protocol + '//' + window.location.hostname
+            + window.location.pathname;
+    }
+
+    /**
+     * Gets all params from the url query string
+     *
+     * @return {Object}
+     */
+    me.getUrlParams = function(){
+        var params = {};
+
+        if (location.search) {
+            var parts = location.search.substring(1).split('&');
+
+            for (var i = 0; i < parts.length; i++) {
+                var nv = parts[i].split('=');
+                if (!nv[0]) continue;
+                params[nv[0]] = nv[1] || true;
+            }
+        }
+        return params;
     }
 }
