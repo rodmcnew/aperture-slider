4a6c12f1ab2b157f2526026f109e64202bbcd03b
Rod Mcnew <rmcnew@relivinc.com>
2013-01-08 14:00:31 -0600

    Update from RCM project

--- update-from-rcm.bash
+++
@@ -1,5 +0,0 @@
-#This script isn't really part of the project, it just makes things easier for Rod
-cp /www/sites/reliv/application/vendor/reliv/Rcm/public/vendor/aperture-slider/* ~/aperture-slider/
-git add .
-git commit -m "Update from RCM project"
-git push origin master
