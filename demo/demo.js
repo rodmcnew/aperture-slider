0e215ce3891b8d2d0ee4339e55e1936fe13a5666
Rod Mcnew <rmcnew@relivinc.com>
2013-06-11 09:42:35 -0500

    format and cleanup code

--- demo/demo.js
+++ demo/demo.js
@@ -13,12 +13,12 @@ $(function () {
     //Init the slider
     var slideDiv = $('.apertureSlider');
     var configOverrides = {
-        minHeight:200,
-        backButtonSupport:true
+        minHeight: 200,
+        backButtonSupport: true
     };
     var apertureSlider = new ApertureSlider(slideDiv, configOverrides);
 
-    //Attach forward button click event handler
+    //Attach continue button click event handler
     $('input[value="Continue"]').click(apertureSlider.goForward);
 
     //Setup optional progress indicator
@@ -32,13 +32,6 @@ $(function () {
             progressIndicator.setProgress(
                 apertureSlider.getCurrentFrame()
             )
-        }
-    );
-
-    //Attach tab click event handler
-    $('.progressIndicator span').click(
-        function () {
-            apertureSlider.goToFrame($(this).html());
         }
     );
 });
